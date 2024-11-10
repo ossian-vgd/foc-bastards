ef0506a5172ca5c835fe9002cb3ab20e162ec5e2
shanesamuelson <shane.o.samuelson@gmail.com>
2024-11-10 07:53:35 -0600

    Added logic for taking leadership as input

--- troopCalc.py
+++ troopCalc.py
@@ -1,7 +1,6 @@
-print("hello world")
-print("hello shane")
+print("--------------TROOP CALCULATOR-----------------------")
 
-totalLeadership = 50000;
+leadership = 248000
 
 # the lookup is strength, health, leadership
 statLookup = {
@@ -10,39 +9,43 @@ statLookup = {
 }
 
 countLookup = {
-    'MELEE-S-8' : 460,
-    'MOUNT-S-8' : 225,
-    'RANGE-S-8' : 440,
-    'FLY-S-8' : 21,
+    'MELEE-S-8' : [470,1],
+    'MOUNT-S-8' : [230,2],
+    'RANGE-S-8' : [450,1],
+    'FLY-S-8' : [21,20],
     
-    'MELEE-S-9' : 225,
-    'MOUNT-S-9' : 112,
-    'RANGE-S-9' : 220,
-    'FLY-S-9' : 10,
+    'MELEE-S-9' : [225,1],
+    'MOUNT-S-9' : [112,2],
+    'RANGE-S-9' : [220,1],
+    'FLY-S-9' : [10,20],
     
-    'MELEE-G-8' : 360,
-    'MOUNT-G-8' : 175,
-    'RANGE-G-8' : 345,
-    'FLY-G-8' : 17,
+    'MELEE-G-8' : [360,1],
+    'MOUNT-G-8' : [175,2],
+    'RANGE-G-8' : [345,1],
+    'FLY-G-8' : [17,20],
 
-    'MELEE-G-9' : 185,
-    'MOUNT-G-9' : 90,
-    'RANGE-G-9' : 175,
-    'FLY-G-9' : 8
+    'MELEE-G-9' : [185,1],
+    'MOUNT-G-9' : [90,2],
+    'RANGE-G-9' : [175,1],
+    'FLY-G-9' : [8,20]
     
 }
+
+totalVal = 0
+for key,value in countLookup.items():
+    totalVal += value[0]*value[1]
+
+percentLookup = {}
+for key,value in countLookup.items():
+    percentLookup[key] = (value[0]*value[1])/totalVal
+
 
 kill_1 = 'MELEE'
 kill_2 = 'FLY'
 kill_3 = 'MOUNT'
 kill_4 = 'RANGE'
 
-# for a 250 multiplier I did 170 M9 and 350 M8 and up to 500 M7
-# 250 for shadow city with herc/beo/cleo
-factor = 250 
-
-# factor = 180 for swarm with herc/beo/cleo 120 m9, 250 m8, 400 m7
-for key,value in countLookup.items():
-    count = value*factor
-    print(key + ":" + str(count))
+for key,value in percentLookup.items():
+    count = (leadership*value)/countLookup[key][1]
+    print(key + ":" + str(round(count)))
 
