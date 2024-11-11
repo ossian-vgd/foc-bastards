8cf1615ec01c7e02041486ea20d56c2d6a2211ce
shanesamuelson <shane.o.samuelson@gmail.com>
2024-11-11 16:20:22 -0600

    refactored for battle plan

--- troopCalc.py
+++ troopCalc.py
@@ -5,11 +5,12 @@ import os
 print("TOTAL LEADERSHIP:"+sys.argv[1])
 print("-----------------------------------------------------")
 leadership = int(sys.argv[1])
-dominance = 40000
+battleplan = sys.argv[2]
 
-def calcResults(leadval):
-  
+def calcResults(leadval, battleplan):
+
   leadval = int(leadval)
+  selectbattleplan = battleplan
 
   # the lookup is strength, health, leadership
   statLookup = {
@@ -21,50 +22,52 @@ def calcResults(leadval):
 
   # This lookup table has kill order from top to bottom
   # The values are number of troops, leadership
-  countLookup = {
+
+  countLookup_BP1 = {
     'MELEE-S8' : [470,1],
     'MOUNT-S8' : [230,2],
     'RANGE-S8' : [450,1],
-    'FLY-S8' : [21,20],
-    
+    'FLY-S8'   : [21,20],
+
     'MELEE-S9' : [225,1],
     'MOUNT-S9' : [112,2],
     'RANGE-S9' : [220,1],
-    'FLY-S9' : [10,20],
-    
+    'FLY-S9'   : [10,20],
+
     'MELEE-G8' : [360,1],
     'MOUNT-G8' : [175,2],
     'RANGE-G8' : [345,1],
-    'FLY-G8' : [17,20],
+    'FLY-G8'   : [17,20],
 
     'MELEE-G9' : [185,1],
     'MOUNT-G9' : [90,2],
     'RANGE-G9' : [175,1],
-    'FLY-G9' : [8,20]
+    'FLY-G9'   : [8,20]
   }
 
   battleLookup = {}
-  battleLookup['S8:S9:G8:G9:MELEE:MOUNT:RANGE:FLY'] = countLookup 
+  battleLookup['S8:S9:G8:G9:MELEE:MOUNT:RANGE:FLY'] = countLookup_BP1
+  battleLookup['S8:S9:G8:G9:MELEE:FLY:MOUNT:RANGE'] = countLookup_BP1
 
   totalVal = 0
-  for key,value in battleLookup['S8:S9:G8:G9:MELEE:MOUNT:RANGE:FLY'].items():
+  for key,value in battleLookup[selectbattleplan].items():
     totalVal += value[0]*value[1]
 
   percentLookup = {}
-  for key,value in countLookup.items():
+  for key,value in battleLookup[selectbattleplan].items():
     percentLookup[key] = (value[0]*value[1])/totalVal
 
   finalCountLookup = {}
   msg = "<table border=\"1\" style=\"width:25%\"><tr><th align=\"left\">Troop Type</th><th align=\"left\">Count</th></tr>"
   for key,value in percentLookup.items():
-    count = (leadval*value)/countLookup[key][1]
+    count = (leadval*value)/battleLookup[selectbattleplan][key][1]
     finalCountLookup[key] = count
-    msg += "<tr><td>"+key + "</td><td>" + str(round(count)) + "</td></tr>" 
+    msg += "<tr><td>"+key + "</td><td>" + str(round(count)) + "</td></tr>"
 
   #--------------Print out monster count ------------------
   monsterCount = (0.9*statLookup['FLY-G9'][1]*finalCountLookup['FLY-G9'])/statLookup['MELEE-M9'][1]
   msg += "<tr><td>M9</td><td>" + str(math.floor(monsterCount)) + "</td></tr></table>"
-  return msg 
+  return msg
 
-retVal = calcResults(leadership)
+retVal = calcResults(leadership,battleplan)
 print(retVal)
