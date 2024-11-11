dd7b1ffc1a139fe4f2077770b2c7981586501fef
shanesamuelson <shane.o.samuelson@gmail.com>
2024-11-10 22:57:36 -0600

    clean up output to be html table

--- troopCalc.py
+++ troopCalc.py
@@ -7,42 +7,40 @@ print("-----------------------------------------------------")
 leadership = int(sys.argv[1])
 dominance = 40000
 
-if "GITHUB_STEP_SUMMARY" in os.environ:
- def print_to_summary(message):
-    with open(os.environ['GITHUB_STEP_SUMMARY'],'a') as f:
-        f.write(message + '\n')
-
 def calcResults(leadval):
+  
+  leadval = int(leadval)
+
   # the lookup is strength, health, leadership
   statLookup = {
-    'MELEE-G-9' : (5510,16530,1),
-    'FLY-G-9'   : (110200,330600,20),
-    'MELEE-G-8' : (3060,9180,1),
-    'MELEE-M-9' : (1210000,3630000,55)
+    'MELEE-G9' : (5510,16530,1),
+    'FLY-G9'   : (110200,330600,20),
+    'MELEE-G8' : (3060,9180,1),
+    'MELEE-M9' : (1210000,3630000,55)
   }
 
   # This lookup table has kill order from top to bottom
   # The values are number of troops, leadership
   countLookup = {
-    'MELEE-S-8' : [470,1],
-    'MOUNT-S-8' : [230,2],
-    'RANGE-S-8' : [450,1],
-    'FLY-S-8' : [21,20],
+    'MELEE-S8' : [470,1],
+    'MOUNT-S8' : [230,2],
+    'RANGE-S8' : [450,1],
+    'FLY-S8' : [21,20],
     
-    'MELEE-S-9' : [225,1],
-    'MOUNT-S-9' : [112,2],
-    'RANGE-S-9' : [220,1],
-    'FLY-S-9' : [10,20],
+    'MELEE-S9' : [225,1],
+    'MOUNT-S9' : [112,2],
+    'RANGE-S9' : [220,1],
+    'FLY-S9' : [10,20],
     
-    'MELEE-G-8' : [360,1],
-    'MOUNT-G-8' : [175,2],
-    'RANGE-G-8' : [345,1],
-    'FLY-G-8' : [17,20],
+    'MELEE-G8' : [360,1],
+    'MOUNT-G8' : [175,2],
+    'RANGE-G8' : [345,1],
+    'FLY-G8' : [17,20],
 
-    'MELEE-G-9' : [185,1],
-    'MOUNT-G-9' : [90,2],
-    'RANGE-G-9' : [175,1],
-    'FLY-G-9' : [8,20]
+    'MELEE-G9' : [185,1],
+    'MOUNT-G9' : [90,2],
+    'RANGE-G9' : [175,1],
+    'FLY-G9' : [8,20]
   }
 
   battleLookup = {}
@@ -57,22 +55,16 @@ def calcResults(leadval):
     percentLookup[key] = (value[0]*value[1])/totalVal
 
   finalCountLookup = {}
-  msg = ""
+  msg = "<table border=\"1\" style=\"width:25%\"><tr><th align=\"left\">Troop Type</th><th align=\"left\">Count</th></tr>"
   for key,value in percentLookup.items():
     count = (leadval*value)/countLookup[key][1]
     finalCountLookup[key] = count
-    msg += key + ":" + str(round(count)) + "<br>" 
-  #   print(msg)
-  #    if "GITHUB_STEP_SUMMARY" in os.environ:
-  #      print_to_summary(msg)
+    msg += "<tr><td>"+key + "</td><td>" + str(round(count)) + "</td></tr>" 
 
   #--------------Print out monster count ------------------
-  monsterCount = (0.9*statLookup['FLY-G-9'][1]*finalCountLookup['FLY-G-9'])/statLookup['MELEE-M-9'][1]
-  msg += "Monster M9 count:" + str(math.floor(monsterCount)) + "<br>"
+  monsterCount = (0.9*statLookup['FLY-G9'][1]*finalCountLookup['FLY-G9'])/statLookup['MELEE-M9'][1]
+  msg += "<tr><td>M9</td><td>" + str(math.floor(monsterCount)) + "</td></tr></table>"
   return msg 
-  #print(msg)
-  #if "GITHUB_STEP_SUMMARY" in os.environ:
-  #  print_to_summary(msg)
 
 retVal = calcResults(leadership)
 print(retVal)
