20013c47418e146e73f2a1b719a3e1667d102115
ossian-bst <139840841+ossian-vgd@users.noreply.github.com>
2025-01-18 09:47:36 -0600

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 
 class MyForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
+    name = StringField('Leadership', validators=[DataRequired()])
+    dropdown = SelectField('Choose an option', choices=['S8:G8:S9:G9:MELEE:FLY:MOUNT:RANGE','S8:S9:G8:G9:MELEE:MOUNT:RANGE:FLY', 'S8:S9:G8:G9:MELEE:FLY:MOUNT:RANGE','SKADI:G7:G8:G9:S9:MELEE:FLY:MOUNT:RANGE'])
     submit = SubmitField('Submit')
